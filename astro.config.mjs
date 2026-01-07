d5d7d7fde0f636a5808b4c1e78e59a146b474bc8
karinamatarritaobando-alt <karinamatarritaobando@gmail.com>
2026-01-06 19:39:05 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://karinamatarrita-alt.github.io',
+  base: '/portfolio',
+
   vite: {
-      plugins: [tailwindcss()],
+    plugins: [tailwindcss()],
   },
 
-  integrations: [react()]
+  integrations: [react()],
 });
